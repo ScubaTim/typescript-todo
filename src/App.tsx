7e51ed0b1e032267495be426c828e36df7255db2
Tim Gentry <gentry.tim@gmail.com>
2020-11-12 15:42:47 -0500

    added NewTodo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-import './App.css';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }]
+
+  const todoAddHandler = (text: string) => {
+    console.log(text)
+  }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
